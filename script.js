18b8519b4aeb83c2d03ca68d47b045e30c733bf2
Shiwani-325 <shrivastavashivani325@gmail.com>
2022-06-24 00:32:50 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,58 @@
+$(document).ready(function(){
+
+     $('#menu').click(function(){
+       $(this).toggleClass('fa-times');
+       $('header').toggleClass('toggle');
+     });
+   
+     $(window).on('scroll load',function(){
+   
+       $('#menu').removeClass('fa-times');
+       $('header').removeClass('toggle');
+   
+       if($(window).scrollTop() > 0){
+         $('.top').show();
+       }else{
+         $('.top').hide();
+       }
+   
+     });
+   
+     // smooth scrolling 
+   
+     $('a[href*="#"]').on('click',function(e){
+   
+       e.preventDefault();
+   
+       $('html, body').animate({
+   
+         scrollTop : $($(this).attr('href')).offset().top,
+   
+       },
+         500, 
+         'linear'
+       );
+   
+     });
+   
+   });
+
+   function sendEmail(){
+     Email.send({
+         Host:"smtp.gmail.com" ,
+         username:"azadvocalss@gmail.com",
+         password: "9939264896",
+         To:'azadvocalss@gmail.com',
+         From:document.getElementById("email").value,
+         subject:"New Contact for enquiry",
+        Body:"Name: " + document.getElementById("name").value
+         +"<br> Email:"+document.getElementById("email").value 
+         +"<br> Contact no:"+document.getElementById("contact no.").value 
+         +"<br> Message:"+document.getElementById("message").value
+     }).then(
+
+      message=>alert("message sent successfully")
+     );
+   }
+
+
