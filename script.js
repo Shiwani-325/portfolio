dc4e338a51d9f57e3c5c53f72472e993c7a507a7
Shiwani <96593318+Shiwani-325@users.noreply.github.com>
2022-07-11 00:24:39 +0530

    Updated

--- script.js
+++ script.js
@@ -1,58 +1,49 @@
-$(document).ready(function(){
+$(document).ready(function () {
+  $("#menu").click(function () {
+    $(this).toggleClass("fa-times");
+    $("header").toggleClass("toggle");
+  });
 
-     $('#menu').click(function(){
-       $(this).toggleClass('fa-times');
-       $('header').toggleClass('toggle');
-     });
-   
-     $(window).on('scroll load',function(){
-   
-       $('#menu').removeClass('fa-times');
-       $('header').removeClass('toggle');
-   
-       if($(window).scrollTop() > 0){
-         $('.top').show();
-       }else{
-         $('.top').hide();
-       }
-   
-     });
-   
-     // smooth scrolling 
-   
-     $('a[href*="#"]').on('click',function(e){
-   
-       e.preventDefault();
-   
-       $('html, body').animate({
-   
-         scrollTop : $($(this).attr('href')).offset().top,
-   
-       },
-         500, 
-         'linear'
-       );
-   
-     });
-   
-   });
+  $(window).on("scroll load", function () {
+    $("#menu").removeClass("fa-times");
+    $("header").removeClass("toggle");
 
-   function sendEmail(){
-     Email.send({
-         Host:"smtp.gmail.com" ,
-         username:"azadvocalss@gmail.com",
-         password: "9939264896",
-         To:'azadvocalss@gmail.com',
-         From:document.getElementById("email").value,
-         subject:"New Contact for enquiry",
-        Body:"Name: " + document.getElementById("name").value
-         +"<br> Email:"+document.getElementById("email").value 
-         +"<br> Contact no:"+document.getElementById("contact no.").value 
-         +"<br> Message:"+document.getElementById("message").value
-     }).then(
+    if ($(window).scrollTop() > 0) {
+      $(".top").show();
+    } else {
+      $(".top").hide();
+    }
+  });
 
-      message=>alert("message sent successfully")
-     );
-   }
+  // smooth scrolling
 
+  $('a[href*="#"]').on("click", function (e) {
+    e.preventDefault();
 
+    $("html, body").animate(
+      {
+        scrollTop: $($(this).attr("href")).offset().top,
+      },
+      500,
+      "linear"
+    );
+  });
+});
+
+//  function sendEmail(){
+//    Email.send({
+//         Host:"smtp.gmail.com" ,
+//         username:"azadvocalss@gmail.com",
+//        password: "9939264896",
+//        To:'azadvocalss@gmail.com',
+//         From:document.getElementById("email").value,
+//         subject:"New Contact for enquiry",
+//        Body:"Name: " + document.getElementById("name").value
+//        +"<br> Email:"+document.getElementById("email").value
+//        +"<br> Contact no:"+document.getElementById("contact no.").value
+//        +"<br> Message:"+document.getElementById("message").value
+//     }).then(
+
+//     message=>alert("message sent successfully")
+//    );
+//  }
